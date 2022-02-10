e7e560a224536d29fb9a707cb8225dc1e1794a4f
SkylarZhang99 <yz85965@uga.edu>
2022-02-10 11:33:26 -0500

    added the file for neumann boundary condition

--- WG_test_NeumannBC.m
+++ WG_test_NeumannBC.m
@@ -13,8 +13,8 @@ right=node(2:end);
 DoFs = 2*Num+NO;  %number of base functions
 bc = 2*Num+[1,NO];  %boundary 
 free = setdiff([1:DoFs],bc);   
-alpha=0; %dirichlet boundary condition 
-beta=0;  %dirichlet boundary condition 
+alpha=0; %neumann boundary condition 
+beta=0;  %nuemann boundary condition 
 
 
 G = h*[1/5 1/4 1/3; 1/4 1/3 1/2; 1/3 1/2 1];
@@ -27,9 +27,9 @@ Mt = [h/3 h/6 0 0;h/6 h/3 0 0;0 0 0 0; 0 0 0 0];
 A = sparse(DoFs,DoFs);
 M = sparse(DoFs,DoFs); 
 b = sparse(DoFs,1);
-d = sparse (DoFs,1); 
-d(2*Num+1,1)= alpha;   %dirichelet boundary condition 
-d(end,1) = beta;       %dirichelet boundary condition
+n = sparse (DoFs,1); 
+n(2*Num+1,1)= -alpha;   %neumann boundary condition 
+n(end,1) = beta;       %neumann boundary condition
 for i = 1:Num
     Index = [2*i-1,2*i,2*Num+elem(i,:)];
 %     A = A + sparse(ones(4,1)*Index,Index'*ones(1,4),At,DoFs,DoFs);
@@ -39,22 +39,11 @@ for i = 1:Num
      M = M + sparse(repmat(Index,4,1),repmat(Index',1,4),Mt,DoFs,DoFs);
 end
 
-% w=[5/9;8/9;5/9];
 nq = 7;
 [q,w] = lgwt(nq,-1,1);
-% w = [1; 1];
-% q = [-1/sqrt(3);1/sqrt(3)]; 
-%q = [(3-sqrt(3))/6;(3+sqrt(3))/6];
-%phi=[(3-sqrt(3))/6 (3+sqrt(3))/6;(3+sqrt(3))/6 (3-sqrt(3))/6 ];
-% phi=[(3+sqrt(3))/6 (3-sqrt(3))/6;(3-sqrt(3))/6 (3+sqrt(3))/6 ];
-% phi=[(1+sqrt(3/5))/2 1/2 (1-sqrt(3/5))/2; (1-sqrt(3/5))/2 1/2 (1+sqrt(3/5))/2];
 phi = [(1+q)'/2;(1-q)'/2];
-% q = [node(:,1:Num) ;node(:,1:Num)]' + h/2 + h/2*repmat(q',Num,1); %matrix for the quadrature points
-%q = h*repmat(q',Num,1) + [node(:,1:Num) ;node(:,1:Num)]';
-% q=[-sqrt(3/5);0;sqrt(3/5)]; %trying to set up 3 points quadrature 
-% q = [node(:,1:Num) ;node(:,1:Num);node(:,1:Num)]' + h/2 + h/2*repmat(q',Num,1);
 q = repmat(node(:,1:Num),nq,1)' + h/2 + h/2*repmat(q',Num,1);
-% nq = size(w,1);
+
 
 % computing the right hand side 
 f=@(x) pi^2*sin(pi*x);  %f(q) col is qua points row is intervals 
@@ -62,9 +51,11 @@ f=@(x) pi^2*sin(pi*x);  %f(q) col is qua points row is intervals
 %f=@(x) -(12*x.^2-12*x+2);
 F=h/2*phi*(f(q)'.*w); % check h/2
 b(1:2*Num)=F(:);
+RHS = b-n;  %neumann boundary condition
 % ufree = A(free,free)\b(free);
 u = zeros(DoFs,1);
-u(free) = A(free,free)\b(free);
+u(end)=1; %fixing one coefficient to guarantee linear independency
+u(free) = A(free,free)\RHS(free);  %solution with neumann boundary condition
 % u(bc) = [0; 0];
 
 plot(node,u(2*Num+1:end),'*','LineWidth',2,'MarkerSize',10)% ub
@@ -77,8 +68,6 @@ plot([node(elem(:,1))',node(elem(:,2))']',[u(2:2:2*Num),u(1:2:2*Num)]','-','Line
 exactu(q);
 eta2=(repmat(right',1,nq)-q)/h;
 eta1=ones(Num,nq)-eta2;
-%eta2=repmat([(1+sqrt(3/5))/2, 1/2,(1-sqrt(3/5))/2],Num,1);
-%eta1=ones(Num,3)-eta2;     eta 1 and 2 using 3 points quadrature 
 T1=eta1.*exactu(q);
 T2=eta2.*exactu(q);
 temp1=h/2*T1*w;  %h needs to be divided by 2 here by the formula for gaussian quadrature 
